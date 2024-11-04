84c8eaaeb59a27b6277eb6e2fec6177853e3aed8
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-11-04 14:53:58 -0600

    Rubocop compliance.

--- test/controllers/public_controller_test.rb
+++ test/controllers/public_controller_test.rb
@@ -1,22 +1,24 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PublicControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test 'should get home' do
     get root_url
     assert_response :success
   end
 
-  test "should get help" do
+  test 'should get help' do
     get public_help_url
     assert_response :success
   end
 
-  test "should get about" do
+  test 'should get about' do
     get public_about_url
     assert_response :success
   end
 
-  test "should get contact" do
+  test 'should get contact' do
     get public_contact_url
     assert_response :success
   end
