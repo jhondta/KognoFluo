999c472f5cf9dcc2f16afa44a447d9b57d09a7f2
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-11-07 22:38:46 -0600

    FIxes title app in full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,8 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis
   def full_title(page_title = '')
-    base_title = 'Kognado'
-    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+    base_title = 'KognoFluo'
+    page_title.empty? ? base_title : "#{base_title} | #{page_title}"
   end
 
   # Returns the color for the alert
