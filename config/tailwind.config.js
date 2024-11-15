dadaa823f6e68632ac6814a79782f2a4cbbeb465
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-11-15 01:01:26 -0600

    Removes unused animations in Tailwind config.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,20 +13,6 @@ module.exports = {
             fontFamily: {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
-            animation: {
-                'slide-in': 'slideIn 0.3s ease-out',
-                'slide-out': 'slideOut 0.3s ease-out'
-            },
-            keyframes: {
-                slideIn: {
-                    '0%': {transform: 'translateX(100%)', opacity: '0'},
-                    '100%': {transform: 'translateX(0)', opacity: '1'}
-                },
-                slideOut: {
-                    '0%': {transform: 'translateX(0)', opacity: '1'},
-                    '100%': {transform: 'translateX(100%)', opacity: '0'}
-                }
-            }
         },
     },
     plugins: [
