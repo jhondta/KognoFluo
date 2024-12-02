43d6861b061cb426057427fa95543b2b30c6e62b
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-12-01 18:07:40 -0600

    Redirects to create profile before continuing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,7 @@ class ApplicationController < ActionController::Base
   # -- -------------------------------------------------------------------------
   # -- Callbacks ---------------------------------------------------------------
   before_action :authenticate_user!
+  before_action :redirect_to_profile
 
   # -- -------------------------------------------------------------------------
   # -- Helper methods ----------------------------------------------------------
@@ -40,4 +41,18 @@ class ApplicationController < ActionController::Base
   # -- Private Methods ---------------------------------------------------------
 
   private
+
+    # Redirigir a la página de perfil si el usuario está loggeado y no tiene
+    # un perfil.
+    # No redirigir si la página que queremos acceder es la de edición de perfil
+    # o si estamos cerrando sesión.
+    def redirect_to_profile
+      return if !user_signed_in? ||
+        current_user.profile.present? ||
+        (controller_path == 'users/registrations' && ['edit', 'update'].include?(action_name)) ||
+        (controller_path == 'users/sessions' && action_name == 'destroy')
+
+      flash[:error] = t('application.error.no_profile')
+      redirect_to edit_user_registration_path
+    end
 end
