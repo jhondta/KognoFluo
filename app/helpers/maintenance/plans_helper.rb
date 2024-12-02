1bd41b722b3370a5bea1c6b68e4fdce2720f8b47
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-12-02 03:51:10 -0600

    First plan detail view implementation.

--- app/helpers/maintenance/plans_helper.rb
+++ app/helpers/maintenance/plans_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module Maintenance::PlansHelper
+  def format_duration(minutes)
+    return '-' if minutes.blank?
+
+    hours = minutes / 60
+    minutes = minutes % 60
+
+    case
+    when hours > 0 && minutes > 0
+      "#{hours}h #{minutes}m"
+    when hours > 0
+      "#{hours}h"
+    else
+      "#{minutes}m"
+    end
+  end
 end
